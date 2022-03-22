cd8e92f9f2ab05fd707d12b438fd87bcd5e39012
DongHeon Lee <niche3600@gmail.com>
2022-03-22 13:15:28 +0900

    string comparison example

---
+++ Sample5_1_example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sample5_1_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力：");
+            string s = Console.ReadLine();
+            const string HELLO = "Hello";
+            Boolean isEqualString = s.Equals(HELLO, StringComparison.OrdinalIgnoreCase);
+            if (isEqualString) {
+                Console.WriteLine("Helloが入力されました。");
+            }
+            else {
+                Console.WriteLine("Helloと入力してください。");
+            }
+        }
+    }
+}
