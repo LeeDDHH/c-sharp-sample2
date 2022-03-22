bb507357d806f6fa9deed3c7ab482fd0f0c26756
DongHeon Lee <niche3600@gmail.com>
2022-03-22 11:45:12 +0900

    if case example

---
+++ Sample209/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sample209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // キーボードから数値を入力
+            Console.Write("1から3の整数を入力：");
+            int num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else
+            {
+                Console.WriteLine("不適切な値");
+            }
+        }
+    }
+}
