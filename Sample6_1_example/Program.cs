4ae3865e72b34043c4b9d6a7f975a6c5a08a727c
DongHeon Lee <niche3600@gmail.com>
2022-03-22 13:29:13 +0900

    double type variable example

---
+++ Sample6_1_example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sample6_1_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("H2Oの温度：");
+
+            double temp;
+
+            temp = double.Parse(Console.ReadLine());
+
+            if (temp >= 100)
+            {
+                Console.WriteLine("気体");
+            }
+            else if (temp > 0)
+            {
+                Console.WriteLine("液体");
+            }
+            else
+            {
+                Console.WriteLine("固体");
+            }
+        }
+    }
+}
